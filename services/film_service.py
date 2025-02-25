6d8d2e66c3c766dab434d9d72dfe598c3a9ae1d8
Milda Samoskaite <mildasamo@gmail.com>
2025-02-25 22:50:04 +0200

    second commit

--- services/film_service.py
+++ services/film_service.py
@@ -5,26 +5,38 @@ from config import films_path
 class FilmService:
     def __init__(self):
         self.film_list = load_data(films_path)
-    def add_film(self, film_name, film_duration, film_genre, film_director, release_date, age_rating):
 
+    def add_film(self, film_name, film_duration, film_genre, film_director, release_date, age_rating):
         film = Film(film_name, film_duration, film_genre, film_director, release_date, age_rating)
         self.film_list.append(film)
         save_data(films_path, self.film_list)
         print("Filmas sėkmingai pridėtas!")
 
-    def remove_film(self):
-        remove_filmname = input("Įveskite filmo pavadinimą, kurį norite pašalinti: ")
-        
+    def remove_film(self, remove_filmName):
+        film_to_remove = None
+        for film in self.film_list:
+            if film.film_name.lower() == remove_filmName.lower():
+                film_to_remove = film
+                break
+    
+        if film_to_remove:
+            self.film_list.remove(film_to_remove)
+            save_data(films_path, self.film_list)
+            print(f"Filmas '{remove_filmName}' buvo pašalintas.")
+        else:
+            print(f"Filmas '{remove_filmName}' sąraše nerastas.")
 
-        # new_all_films = []
-        # for filmas in self.all_films:
-        #     if ?? != remove_filmname.lower():
+    def search_film(self, wanted):
+        for film in self.film_list:
+            if film.film_name.lower() == wanted.lower() or film.film_director.lower() == wanted.lower():
+                print("Šis filmas yra sąraše")
+                return  # Nutraukti ciklą ir funkciją
+        print("Ieškomas filmas nerastas")  
+
     def display(self):
         for index in range(len(self.film_list)):
             print(f"{index+1}. {self.film_list[index]}")
             
-
-            # TEST GITHUB
 
         
 
--- views/interface.py
+++ views/interface.py
@@ -2,16 +2,48 @@ from services.film_service import FilmService
 
 def festivalis():
     films_service = FilmService()
-    print("Sveiki atvykę į kino festivalio valdymo sistemą!")
-    
 
-    film_name = input("Įveskite filmo pavadinimą: ")
-    film_duration = int(input("Įveskite trukmę minutėmis: "))
-    film_genre = input("Įveskite žanrą: ")
-    film_director = input("Įveskite režisierių: ")
-    release_date = int(input("Įveskite išleidimo metus: "))
-    age_rating = input("Įveskite amžiaus reitingą (pvz., 'N-13'): ")
+    while True:
+        print("--------------------------------------------------")
+        print("\nSveiki atvykę į kino festivalio valdymo sistemą!\n")
+        print("1. Pridėti filmą")
+        print("2. Pašalinti filmą")
+        print("3. Rodyti filmus")
+        print("4. Atnaujinti filmą")
+        print("5. Surasti filmą")
+        print("\nIšeiti - ENTER\n")
+        choice = input("Pasirinkite veiksmą: \n")
 
-    films_service.add_film(film_name, film_duration, film_genre, film_director, release_date, age_rating)
+        if choice == '1':
+            film_name = input("Įveskite filmo pavadinimą: ")
+            film_duration = int(input("Įveskite trukmę minutėmis: "))
+            film_genre = input("Įveskite žanrą: ")
+            film_director = input("Įveskite režisierių: ")
+            release_date = int(input("Įveskite išleidimo metus: "))
+            age_rating = input("Įveskite amžiaus reitingą (pvz., 'N-13'): ")
 
-    films_service.display()
+            films_service.add_film(film_name, film_duration, film_genre, film_director, release_date, age_rating)
+        
+        elif choice == '2':
+            del_filmName = input("Įveskite filmo pavadinimą, kurį norite pašalinti: ")
+
+            films_service.remove_film(del_filmName)
+        
+        elif choice == '3':
+            films_service.display()
+
+        elif choice == '4':
+            pass
+        
+        elif choice == '5':
+            film_exists = input("Norint surasti filmą, įveskite filmo pavadinimą ar filmo režisierių: ")
+            films_service.search_film(film_exists)
+
+        elif choice == '':
+            break
+
+        else:
+            print("Neteisingas pasirinkimas. Bandykite dar kartą.")
+
+        
+       
